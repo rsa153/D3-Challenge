acbec62e21509c2f3a15de2c3b6709d913f142f5
Rana <r-saber@nlaw.northwestern.edu>
2021-06-19 21:58:09 -0500

    create svg wrapper

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,30 @@
-// @TODO: YOUR CODE HERE!
+// create path to data
+var path = "assets/data/data.csv";
+
+// set height and width of svg
+var svgWidth = 960;
+var svgHeight = 500;
+
+// set margins
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// create svg wrapper
+
+var svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chart = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+var xAxis = "age";
